5425913846e19d3104b3d3c44dcc503ddf7be55c
Utopia <izentopia@gmail.com>
2017-09-03 15:37:46 +0800

    新的工程

---
+++ src/main/java/com/utopia/cloudmusicspider/CloudmusicspiderApplication.java
@@ -0,0 +1,12 @@
+package com.utopia.cloudmusicspider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudmusicspiderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudmusicspiderApplication.class, args);
+	}
+}
