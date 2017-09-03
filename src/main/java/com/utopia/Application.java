46c3f6837f776c386bbe0d30437915ad0f9f629c
Utopia <izentopia@gmail.com>
2017-09-03 09:55:36 +0800

    Application

--- src/main/java/com/utopia/Application.java
+++ src/main/java/com/utopia/Application.java
@@ -1,21 +1,20 @@
 package com.utopia;
 
-import com.utopia.service.WebFetcher;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
+import java.io.IOException;
+import java.net.URISyntaxException;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@SpringBootApplication
+@EnableAsync
+@EnableCaching
 public class Application {
 
-    private static Log log = LogFactory.getLog(Application.class);
-
-    public static void main(String[] args){
-
-        String url = "http://music.163.com/#/discover/playlist/?order=hot&cat=%E5%85%A8%E9%83%A8&limit=35&offset=0";
-        WebFetcher webFetcher = new WebFetcher();
-        String result = webFetcher.fetch(url);
-        log.info(result);
-
+    public static void main(String[] args) throws IOException, URISyntaxException {
+        SpringApplication.run(Application.class);
     }
-
 
 }
